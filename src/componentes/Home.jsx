d2e5b8ee7f845b99cf6f2dde7c13cb13fd67250d
andrearodriguezg <andrearodriguezg95@gmail.com>
2021-12-02 19:46:41 -0500

    Primer cambio

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,19 @@
+export function Home(){
+
+    return(
+        <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Laboratorio</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    
+    )
+}
