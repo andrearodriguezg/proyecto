ed3006d61610481235fd52e4a905bbffec167388
andrearodriguezg <andrearodriguezg95@gmail.com>
2021-12-02 19:54:02 -0500

    Creación de rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './componentes/home';
+import { Login } from './componentes/Login';
+import { Registrarse } from './componentes/Registrarse';
+import { HomeUsuarioint } from './componentes/HomeUsuarioint';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter> 
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Login" element={<Login/>} />
+      <Route path="/Registrarse" element={<Registrarse/>}/>
+      <Route path="/HomeUsuarioint" element={<HomeUsuarioint/>}/>
+    
+    </Routes>
+    </BrowserRouter>
+    
+    </>
   );
 }
 
 export default App;
+//<div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
